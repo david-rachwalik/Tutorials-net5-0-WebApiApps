0c6326734e8dd80e2ef907723b0abd3a1b4b2660
David Rachwalik <david.rachwalik@outlook.com>
2021-04-05 06:13:58 -0500

    Prevent over-posting of TodoItem

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -5,10 +5,18 @@ using System.Threading.Tasks;
 
 namespace TodoApi.Models
 {
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+
     public class TodoItem
     {
         public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
+        public string Secret { get; set; }
     }
 }
